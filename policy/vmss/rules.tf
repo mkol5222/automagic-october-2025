52238014bab4e352526a2dec235407e4215542d1
Martin Koldovsky <mkoldov@gmail.com>
2025-09-27 18:02:56 +0000

    policy vmss ingress

--- policy/vmss/rules.tf
+++ policy/vmss/rules.tf
@@ -49,3 +49,35 @@ resource "checkpoint_management_access_rule" "vnet_egress" {
   vpn             = "Any"
   action          = "Accept"
 }
+
+### VNETs ingress section
+resource "checkpoint_management_access_section" "vnet_ingress_section" {
+  name = "VNETs ingress section"
+  position = {above  = checkpoint_management_access_section.vnet_egress_section.id}
+   layer = local.layer_name
+}
+
+# allow SSH to instances
+resource "checkpoint_management_access_rule" "vnet_ingress" {
+  name        = "VMSS instances ingress"
+  layer       = local.layer_name
+  position    = { below = checkpoint_management_access_section.vnet_ingress_section.id }
+  source      = ["Any"]
+  destination = [checkpoint_management_dynamic_object.LocalGatewayExternal.id]
+  service     = ["ssh_version_2"]
+  content     = ["Any"]
+  time        = ["Any"]
+  install_on  = ["Policy Targets"]
+  track = {
+    type                    = "Log"
+    accounting              = false
+    alert                   = "none"
+    enable_firewall_session = false
+    per_connection          = true
+    per_session             = false
+  }
+  action_settings = {}
+  custom_fields   = {}
+  vpn             = "Any"
+  action          = "Accept"
+}
