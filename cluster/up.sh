81a1308cccc7c946b6fcd2842aa3025ccc08a694
Martin Koldovsky <mkoldov@gmail.com>
2025-09-29 06:37:16 +0000

    cluster basics

---
+++ cluster/up.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euo pipefail
+
+. ./tfvars.sh
+
+dotenvx run -f ../.env -fk ../.env.keys -- terraform init
+
+dotenvx run -f ../.env -fk ../.env.keys -- terraform apply -auto-approve
---
+++ cluster/variables.tf
@@ -0,0 +1,24 @@
+variable "envId" {
+  description = "Environment ID"
+  type        = string
+}
+
+variable "subscriptionId" {
+  description = "The Subscription ID which should be used."
+  type        = string
+}
+
+variable "tenant" {
+  description = "The Tenant ID which should be used."
+  type        = string
+}
+
+variable "cluster_admin_password" {
+  description = "Cluster Admin Password"
+  type        = string
+}
+
+variable "cluster_sic_key" {
+  description = "Cluster SIC Key"
+  type        = string
+}
