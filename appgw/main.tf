65685a8a67806dc2f8550a86bc0821613fd39f00
Martin Koldovsky <mkoldov@gmail.com>
2025-10-13 08:35:58 +0000

    appgw

--- appgw/main.tf
+++ appgw/main.tf
@@ -38,76 +38,76 @@ data "azurerm_virtual_network" "vnet" {
 #   address_prefixes     = ["10.254.0.0/24"]
 # }
 
-# resource "azurerm_public_ip" "example" {
-#   name                = "example-pip"
-#   resource_group_name = azurerm_resource_group.example.name
-#   location            = azurerm_resource_group.example.location
-#   allocation_method   = "Static"
-# }
+resource "azurerm_public_ip" "appgw" {
+  name                = "appgw-pip"
+  resource_group_name = azurerm_resource_group.appgw.name
+  location            = azurerm_resource_group.appgw.location
+  allocation_method   = "Static"
+}
 
 # # since these variables are re-used - a locals block makes this more maintainable
-# locals {
-#   backend_address_pool_name      = "${azurerm_virtual_network.example.name}-beap"
-#   frontend_port_name             = "${azurerm_virtual_network.example.name}-feport"
-#   frontend_ip_configuration_name = "${azurerm_virtual_network.example.name}-feip"
-#   http_setting_name              = "${azurerm_virtual_network.example.name}-be-htst"
-#   listener_name                  = "${azurerm_virtual_network.example.name}-httplstn"
-#   request_routing_rule_name      = "${azurerm_virtual_network.example.name}-rqrt"
-#   redirect_configuration_name    = "${azurerm_virtual_network.example.name}-rdrcfg"
-# }
+locals {
+  backend_address_pool_name      = "${data.azurerm_virtual_network.vnet.name}-beap"
+  frontend_port_name             = "${data.azurerm_virtual_network.vnet.name}-feport"
+  frontend_ip_configuration_name = "${data.azurerm_virtual_network.vnet.name}-feip"
+  http_setting_name              = "${data.azurerm_virtual_network.vnet.name}-be-htst"
+  listener_name                  = "${data.azurerm_virtual_network.vnet.name}-httplstn"
+  request_routing_rule_name      = "${data.azurerm_virtual_network.vnet.name}-rqrt"
+  redirect_configuration_name    = "${data.azurerm_virtual_network.vnet.name}-rdrcfg"
+}
 
-# resource "azurerm_application_gateway" "network" {
-#   name                = "example-appgateway"
-#   resource_group_name = azurerm_resource_group.example.name
-#   location            = azurerm_resource_group.example.location
+resource "azurerm_application_gateway" "network" {
+  name                = "appgw-${local.envId}"
+  resource_group_name = azurerm_resource_group.appgw.name
+  location            = azurerm_resource_group.appgw.location
 
-#   sku {
-#     name     = "Standard_v2"
-#     tier     = "Standard_v2"
-#     capacity = 2
-#   }
+  sku {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
+  }
 
-#   gateway_ip_configuration {
-#     name      = "my-gateway-ip-configuration"
-#     subnet_id = azurerm_subnet.example.id
-#   }
+  gateway_ip_configuration {
+    name      = "my-gateway-ip-configuration"
+    subnet_id = azurerm_subnet.appgw.id
+  }
 
-#   frontend_port {
-#     name = local.frontend_port_name
-#     port = 80
-#   }
+  frontend_port {
+    name = local.frontend_port_name
+    port = 80
+  }
 
-#   frontend_ip_configuration {
-#     name                 = local.frontend_ip_configuration_name
-#     public_ip_address_id = azurerm_public_ip.example.id
-#   }
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.appgw.id
+  }
 
-#   backend_address_pool {
-#     name = local.backend_address_pool_name
-#   }
+  backend_address_pool {
+    name = local.backend_address_pool_name
+  }
 
-#   backend_http_settings {
-#     name                  = local.http_setting_name
-#     cookie_based_affinity = "Disabled"
-#     path                  = "/path1/"
-#     port                  = 80
-#     protocol              = "Http"
-#     request_timeout       = 60
-#   }
+  backend_http_settings {
+    name                  = local.http_setting_name
+    cookie_based_affinity = "Disabled"
+    path                  = "/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+  }
 
-#   http_listener {
-#     name                           = local.listener_name
-#     frontend_ip_configuration_name = local.frontend_ip_configuration_name
-#     frontend_port_name             = local.frontend_port_name
-#     protocol                       = "Http"
-#   }
+  http_listener {
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
+    protocol                       = "Http"
+  }
 
-#   request_routing_rule {
-#     name                       = local.request_routing_rule_name
-#     priority                   = 9
-#     rule_type                  = "Basic"
-#     http_listener_name         = local.listener_name
-#     backend_address_pool_name  = local.backend_address_pool_name
-#     backend_http_settings_name = local.http_setting_name
-#   }
-# }
+  request_routing_rule {
+    name                       = local.request_routing_rule_name
+    priority                   = 9
+    rule_type                  = "Basic"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name
+    backend_http_settings_name = local.http_setting_name
+  }
+}
